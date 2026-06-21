ec18a76aa966a39ced732923c8f2008f0a638672
csy <csy>
2026-06-21 21:23:16 +0200

    Seems to be an artifical limit and anomaly exceeded it

--- src/xrCore/xrstring.cpp
+++ src/xrCore/xrstring.cpp
@@ -157,7 +157,7 @@ str_value* str_container::dock(pcstr value) const
     // calc len
     const auto s_len = xr_strlen(value);
     const auto s_len_with_zero = s_len + 1;
-    VERIFY(sizeof(str_value) + s_len_with_zero < 4096);
+    VERIFY(sizeof(str_value) + s_len_with_zero < 8192);
 
     // setup find structure
     char header[sizeof(str_value)];
