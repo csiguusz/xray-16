0f66d43df2b7a1551e32c0f4030ffab46d347aa7
csy <csy>
2026-06-21 21:25:48 +0200

    Allow early querys for game time factor

--- src/xrGame/Level.cpp
+++ src/xrGame/Level.cpp
@@ -1015,7 +1015,7 @@ void CLevel::GetGameDateTime(u32& year, u32& month, u32& day, u32& hours, u32& m
 
 float CLevel::GetGameTimeFactor()
 {
-    return game->GetGameTimeFactor();
+    return (game ? game->GetGameTimeFactor() : 1.0f);
 }
 
 void CLevel::SetGameTimeFactor(const float fTimeFactor)
